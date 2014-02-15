604aa4e16ea741730435d6d5e276385009c8e719
taisediass <tdsilva@thoughtworks.com>
2014-02-15 20:37:33 -0300

    Removed comments

--- docpad.js
+++ docpad.js
@@ -2,10 +2,8 @@ module.exports = {
 
   prompts: false,
 
-  // These are variables will be accessible via our templates
   templateData: {
 
-    // Conference info
     conf: {
       name: "QA Night",
       description: "Encontro do grupo Teste de Software-PE",
@@ -16,45 +14,33 @@ module.exports = {
       state: "PE"
     },
 
-    // The Call To Action button at the header,
-    // If you don't want this, just remove the callToAction property.
     callToAction: {
         text: "Inscreva-se",
         link: "https://docs.google.com/forms/d/130ODrcxuQJFCrW8dkcZETJ1m1gYgG9RosekAeSUrhQ4/viewform"
     },
 
-    // Site info
     site: {
       theme: "yellow-swan",
       url: "http://testedesoftware-pe.github.io/conf-boilerplate/",
       googleanalytics: "UA-33656081-1"
     },
 
-    // Active sections on the website
-    // to deactivate comment out with '//'
-    // you can also change order here and it will reflect on page
     sections: [
       'about',
       'location',
       'speakers',
       'schedule',
-     // 'sponsors',
       'partners'
-      // 'contact'
     ],
 
-    // Labels which you can translate to other languages
     labels: {
       about: "Sobre",
       location: "Localização",
       speakers: "Palestrantes",
       schedule: "Agenda",
-      //sponsors: "Sponsors",
       partners: "Parceiros",
-      //contact: "Contato"
     },
 
-    // The entire schedule
     schedule: [
       {
         name: "Check-in",
@@ -81,10 +67,6 @@ module.exports = {
         photo: "themes/yellow-swan/img/leandro.jpg",
         bio: "Leandro possui formação em Análise e Desenvolvimento de Software e trabalha com testes de software desde 2005. Atualmente, trabalha como Analista de Testes na Pitangi",
         company: "Pitang Consultoria e Sistemas",
-        //link: {
-        //  href: "http://github.com/billy95",
-        //  text: "@billy95"
-        //},
         presentation: {
           title: "Testes Funcionais com Selenium e Java",
           description: "Automação de testes funcionais utilizando o Selenium como framework de automação e Java como liguagem de programação.",
@@ -97,7 +79,6 @@ module.exports = {
         bio: "Rafael é Bacharel em Ciências da Computação, trabalha na área de TI desde 2003: programou em Delphi e PHP e nos últimos 5 anos tem focado em automação de testes. Atualmente é consultor em Qualidade de Software na ThoughtWorks.",
         company: "ThoughtWorks",
         link: {
-          //  href: "http://twitter.com/littlechuck",
           text: "rafalima07@gmail.com"
         },
         presentation: {
@@ -108,16 +89,6 @@ module.exports = {
       }
     ],
 
-    // List of Sponsors
-  //  sponsors: [
-  //    {
-  //      name: "Eventick",
-  //      logo: "themes/yellow-swan/img/sponsor.png",
-  //      url: "http://eventick.com.br"
-  //    }
-  //  ],
-
-    // List of Partners
     partners: [
       {
         name: "ThoughWorks Recife",
@@ -126,7 +97,6 @@ module.exports = {
       }
     ],
 
-    // Theme path
     getTheme: function() {
       return "themes/" + this.site.theme;
     }
