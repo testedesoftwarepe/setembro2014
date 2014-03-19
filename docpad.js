f8da42535191a5cddd8c722d9e5bbea62ca42bf6
Rafael Lima <rlima@thoughtworks.com>
2014-03-19 17:24:44 -0300

    pushing changes to the event

--- docpad.js
+++ docpad.js
@@ -9,7 +9,7 @@ module.exports = {
       description: "Encontro do grupo Teste de Software-PE",     
       topics: "aprendizagem baseada em problemas (PBL) e automação de testes de software", 
       date: "19 de março de 2014",
-      venue: "no CST UNINASSAU",
+      venue: "no CST UNINASSAU (sala 208)",
       address: "Rua João Fernandes Vieira, 110 - Boa Vista",
       city: "Recife",
       state: "PE"
@@ -49,21 +49,7 @@ module.exports = {
       {
         name: "Check-in",
         time: "19h15"
-      },
-      {
-        name: "Ana Claudia Monte",
-        photo: "themes/yellow-swan/img/ana_claudia.jpg",
-        bio: "Ana Claudia possui mestrado em Ciência da Computação pela UFPE na área de Gestão de Processos, graduada em Sistema de Informação pela Faculdade Pernambucana-FAPE. Atua na área de teste desde 2007 com planejamento, projeto, execução e controle de falhas de testes. Professora universitária da Faculdade Pernambucana-FAPE. Atualmente trabalha como Analista de Teste da empresa AGFA HealthCare desde 2010.",
-        company: "AGFA",
-        link: {
-          text: "anaclaudia.monte@gmail.com"
-        },
-        presentation: {
-          title: "Gestão de Processos Aplicada ao Ensino de Teste de Software.",
-          description: "A busca por profissionais qualificados que possuem considerável experiência prática vem crescendo dia-a-dia. Mas, no processo de aprendizagem do ensino tradicional, o aluno é em grande parte um receptor de informações, incluindo conceitos e fundamentos teóricos, onde raramente é dada a prática na resolução de problemas. Portanto, com vista a minimizar este problema, o Ensino de Aprendizagem Baseada em Problemas (PBL) é sugerido.",
-          time: "19h30"
-        }
-      },
+      },      
       {
         name: "Christiano Gonçalves Guerra",
         photo: "themes/yellow-swan/img/christiano.jpeg",
@@ -76,9 +62,23 @@ module.exports = {
         presentation: {
           title: "Testes Funcionais com Selenium e Java",
           description: "Automação de testes funcionais utilizando o Selenium como framework de automação e Java como liguagem de programação.",
-          time: "20:10"
+          time: "19:30"
         }
-      }      
+      },
+      {
+        name: "Ana Claudia Monte",
+        photo: "themes/yellow-swan/img/ana_claudia.jpg",
+        bio: "Ana Claudia possui mestrado em Ciência da Computação pela UFPE na área de Gestão de Processos, graduada em Sistema de Informação pela Faculdade Pernambucana-FAPE. Atua na área de teste desde 2007 com planejamento, projeto, execução e controle de falhas de testes. Professora universitária da Faculdade Pernambucana-FAPE. Atualmente trabalha como Analista de Teste da empresa AGFA HealthCare desde 2010.",
+        company: "AGFA",
+        link: {
+          text: "anaclaudia.monte@gmail.com"
+        },
+        presentation: {
+          title: "Gestão de Processos Aplicada ao Ensino de Teste de Software.",
+          description: "A busca por profissionais qualificados que possuem considerável experiência prática vem crescendo dia-a-dia. Mas, no processo de aprendizagem do ensino tradicional, o aluno é em grande parte um receptor de informações, incluindo conceitos e fundamentos teóricos, onde raramente é dada a prática na resolução de problemas. Portanto, com vista a minimizar este problema, o Ensino de Aprendizagem Baseada em Problemas (PBL) é sugerido.",
+          time: "20h10"
+        }
+      },      
     ],
 
     partners: [
