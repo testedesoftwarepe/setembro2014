2834eb0dcaa2ef83faa5a1e7ac2d806e9dca7bcb
Bruna Chagas <brunabrk@gmail.com>
2014-04-12 11:45:49 -0300

    Adicionando mudancas na agenda

--- docpad.js
+++ docpad.js
@@ -54,21 +54,22 @@ module.exports = {
       {
         name: "Diego Asfora",
         photo: "themes/yellow-swan/img/asfora.jpg",
-        bio: "Trabalha a mais de 7 anos com Agile em diversos projetos e em várias empresas tendo inclusive durante esse período feito mestrado na área de priorização de Requisitos Ágeis. Trabalha a 10 anos com testes de software. Palestrante no Scrum Gathering Brazil em São Paulo 2009.",
-        company: "ThoughWorks",
+        bio: "Trabalha a mais de 7 anos com Agile em diversos projetos e em várias empresas tendo inclusive durante esse período feito mestrado na área de priorização de Requisitos Ágeis. Trabalha há 10 anos com testes de software. Palestrante no Scrum Gathering Brazil em São Paulo 2009.",
+        company: "ThoughtWorks",
         link: {
+          href: "https://twitter.com/diegoasfora",
           text: "@diegoasfora"
         },
         presentation: {
           title: "Testers Técnicos",
-          description: "Introdução qual o papel de um QA Ágil",
+          description: "Overview sobre o papel de um QA. Incluindo algumas práticas, técnicas e ferramentas para contribuir na formação de um QA Ágil.",
           time: "19:30"
         }
       },
 
       {
-        name: "Coffee Break",
-        time: "20h"
+        name: "Intervalo",
+        time: "20:10h"
       },
 
       {
@@ -77,14 +78,18 @@ module.exports = {
         bio: "Henrique é mestre em Ciência da Computação e MBA em Gestão de Pessoas. Foi sócio fundador da FAST Soluções, onde também trabalhou como desenvolvedor, tester, front-end e gerente de projetos. Atualmente, é Eng. de Sistemas no CESAR.",
         company: "CESAR",
         link: {
-          href: "",
+          href: "https://twitter.com/henriqueHbas",
           text: "@henriqueHbas"
         },
         presentation: {
           title: "Specflow + Coypu = BDD em .NET",
-          description: " Uma apresentação no estilo \"live coding\", utilizando Specflow, Gherkin, Coypu, Selenium e NUnit em um problema simples.",
-          time: "20h10"
+          description: "Uma apresentação no estilo \"live coding\", utilizando Specflow, Gherkin, Coypu, Selenium e NUnit em um problema simples.",
+          time: "20h20"
         }
+      },
+      {
+        name: "Encerramento",
+        time: "21:10h"
       }    
     ],
 
@@ -94,11 +99,11 @@ module.exports = {
         logo: "themes/yellow-swan/img/tw-logo.png",
         url: "http://www.thoughtworks.com/pt/"
       },
-      {
+      /*{
         name: "UNINASSAU",
         logo:"themes/yellow-swan/img/uninassau-logo.jpeg",
         url: "http://www.mauriciodenassau.edu.br/"
-      },
+      },*/
       {
         name: "Qualiti",
         logo:"themes/yellow-swan/img/qualiti-logo.jpeg",
