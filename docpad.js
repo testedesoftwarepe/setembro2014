382d26c6f5e0088e26a15a309547f1362ab8f8c6
Rafael <rafalima07@gmail.com>
2014-08-23 15:42:02 -0300

    changes on augusto 2014

--- docpad.js
+++ docpad.js
@@ -7,10 +7,9 @@ module.exports = {
     conf: {
       name: "QA Night",
       description: "Encontro do grupo Teste de Software-PE",
-      topics: "Automando com Java e Selenium VS Ruby e Watir",
-      date: "30 de Julho de 2014",
-      venue: "Qualiti",
-      address: "Av. Marquês de Olinda, 126 - 4º andar - Bairro do Recife",
+      date: "27 de Agosto de 2014",
+      venue: "ThoughtWorks e Hangout on Air",
+      address: "Av. Gov. Agamenon Magalhães, 4779 - Ilha do Leite",
       city: "Recife",
       state: "PE"
     },
@@ -18,19 +17,19 @@ module.exports = {
     callToAction: {
         text: "Inscrições abertas"
         //Comente esse link para que o botão aponte para o próprio site.
-        ,link: "https://docs.google.com/forms/d/1hsRpul583dnw2qUpXJ35swGIH4EEvQKK9dgcrwxNMS0/viewform"
+        ,link: "https://docs.google.com/forms/d/1YB_comM_6X7x6lZVVMUncA7KJEK8kR9UufEcWA57y_Y/viewform"
     },
 
     hangout:{
       hangoutSet: false,
       link:"https://plus.google.com/u/1/events/cm2q6g8adljfr0j7k0sihrk3pcg",
-      hangoutNotSetMessage: "não teremos hangout nesse qa night."
+      hangoutNotSetMessage: "link para hangout em breve."
     },
 
     site: {
       theme: "yellow-swan",
-      url: "http://testedesoftwarepe.github.io/julho2014/",
-      googleanalytics: "UA-53198502-1"
+      url: "http://testedesoftwarepe.github.io/agosto2014/",
+      googleanalytics: "UA-53198502-2"
     },
 
     sections: [
@@ -61,16 +60,16 @@ module.exports = {
       },
 
       {
-        name: "Kildery Sukar",
-        photo: "themes/yellow-swan/img/kildery.jpg",
-        bio: "Kildery é formado em Ciência da Computação pela UNICAP, e trabalha com teste de software desde 2008, focado em análise de requisitos, planejamento e execução de casos de teste, além de desenvolver e executar testes automáticos. Atualmente é engenheiro de software na Modular Mining Systems.",
-        company: "Modular Mining Systems",
+        name: "Rafael Lima",
+        photo: "themes/yellow-swan/img/rafael.JPG",
+        bio: "Rafael é Bacharel em Ciências da Computação, trabalha na área de TI desde 2003: programou em Delphi e PHP e nos últimos 5 anos tem focado em automação de testes. Atualmente é consultor em Qualidade de Software na ThoughtWorks.",
+        company: "ThoughtWorks",
         link: {
-          href: "http://twitter.com/kilderysc",
-          text: "@kilderysc"
+          href: "rafalima07@gmail.com",
+          text: "rafalima07@gmail.com"
         },
         presentation: {
-          title: "O que é Teste de Software?",
+          title: "Por que usar o padrão Page Object?",
           description: "Nesta palestra, vamos falar sobre princípios, processos e gerenciamento de Teste de Software",
           time: "19h15"
         }
